8bd9376061e0ada5a85184652180d38391699290
Sam Nicholson <sam@samn.co.uk>
2013-11-11 13:46:39 +0000

    Assorted fixes and improvements to the database glue script

--- misc/database_glue/tarantula_db_sync.py
+++ misc/database_glue/tarantula_db_sync.py
@@ -3,15 +3,15 @@ import sqlite3
 import os
 
 # Remote database server settings
-db_server = "127.0.0.1"
-db_port = 5433
+db_server = "localhost"
+db_port = 5432
 db_user = "user"
 db_pass = "pass"
-db_name = "database"
+db_name = "db"
 
 # Local database files
 filler_db = "/opt/Tarantula/datafiles/EventProcessor_Fill/filedata.db"
-
+file_db = "/opt/Tarantula/datafiles/EventProcessor_Fill/lazydata.db"
 
 # Connect to remote database
 conn = psycopg2.connect(host=db_server, port=db_port, user=db_user, password=db_pass, database=db_name)
@@ -19,7 +19,7 @@ conn = psycopg2.connect(host=db_server, port=db_port, user=db_user, password=db_
 # Read data for the schedule_fill_items table
 cur = conn.cursor()
 
-cur.execute("SELECT schedule_fill_items.video_id, left(upper(substring(filename from 9)), -4), "
+cur.execute("SELECT schedule_fill_items.video_id, (upper(substring(filename from 9 for (length(filename) - 12)))), "
             "device, item_type, extract('epoch' from duration) * 25, priority, '' "
             "FROM schedule_fill_items "
             "LEFT JOIN video_files ON schedule_fill_items.video_id = video_files.video_id "
@@ -28,7 +28,7 @@ cur.execute("SELECT schedule_fill_items.video_id, left(upper(substring(filename 
             "WHERE video_file_types.mode = 'schedule'")
 result = cur.fetchall()
 
-cur.execute("SELECT videos.id, left(upper(substring(filename from 9)), -4), 'Show', 'show', (extract('epoch' from duration)) * 25, "
+cur.execute("SELECT videos.id, (upper(substring(filename from 9 for (length(filename) - 12)))), 'Show', 'show', (extract('epoch' from duration)) * 25, "
 	"CASE WHEN created_date > (current_date - interval '1 year') THEN '1' "
 	     "WHEN created_date > (current_date - interval '2 years') THEN '5' "
 	     "ELSE '20' "
@@ -41,7 +41,9 @@ cur.execute("SELECT videos.id, left(upper(substring(filename from 9)), -4), 'Sho
 	"WHERE video_file_types.mode = 'schedule' AND duration > interval '0 seconds' "
 	"AND video_id NOT IN "
 		"(SELECT video_id FROM schedule_fill_items) "
-        "GROUP BY videos.id, filename, duration, created_date, video_boxes.display_name, video_boxes.name, video_boxes.url_name")
+        "GROUP BY videos.id, filename, duration, created_date, video_boxes.display_name, video_boxes.name, video_boxes.url_name, "
+        "videos.display_name, videos.url_name "
+	"ORDER BY created_date DESC")
 
 fullvideolist = cur.fetchall()
 
@@ -54,6 +56,12 @@ conn2 = sqlite3.connect(filler_db)
 inscur = conn2.cursor()
 inscur.execute("DELETE FROM items;")
 inscur.executemany("INSERT INTO items (id, name, device, type, duration, weight, description) VALUES (?, ?, ?, ?, ?, ?, ?);", result)
+conn2.commit()
+conn2.close()
+
+conn2 = sqlite3.connect(file_db)
+inscur = conn2.cursor()
+inscur.execute("DELETE FROM items;")
 inscur.executemany("INSERT INTO items (id, name, device, type, duration, weight, description) VALUES (?, ?, ?, ?, ?, ?, ?);", fullvideolist)
 
 conn2.commit()
